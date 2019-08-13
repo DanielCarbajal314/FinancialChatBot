93d0492d654d75c1971b5d6a5c27b70b5edb2ec5
dcarbajal <daniel.carbajal@pucp.edu.pe>
2019-08-13 11:36:51 -0500

    StooqCLient Test Fix

--- Financial.Test.UnitTest.ExternalServices/StookExternalService.cs
+++ Financial.Test.UnitTest.ExternalServices/StookExternalService.cs
@@ -1,18 +1,21 @@
 using Financial.Infrastructure.ExternalServices.StooqClient;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System.Net.Http;
 
 namespace Financial.Test.UnitTest.ExternalServices
 {
     [TestClass]
-    public class UnitTest1
+    public class StookExternalService
     {
         [TestMethod]
         public void StooqClientQuery()
         {
-            //StooqClient stooqClient = new StooqClient(new HttpClient());
-            //var result = stooqClient.QueryStock("aapl.us").Result;
-            //Assert.AreEqual(result.Symbol, "AAPL.US","The Stock information Symbol is Wrong");
+            var httpClientFactoryMock = new Mock<IHttpClientFactory>();
+            httpClientFactoryMock.Setup(x => x.CreateClient(It.IsAny<string>())).Returns(new HttpClient());
+            StooqClient stooqClient = new StooqClient(httpClientFactoryMock.Object);
+            var result = stooqClient.QueryStock("aapl.us").Result;
+            Assert.AreEqual(result.Symbol, "AAPL.US","The Stock information Symbol is Wrong");
         }
     }
 }
